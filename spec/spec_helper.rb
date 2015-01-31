3a3edd3fd580d518db3e71e738db94947fcd608e
Rene Klacan <rene.klacan@reevoo.com>
2015-01-31 19:24:56 +0000

    Ignore code climate if there is no token in ENV

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,10 @@
 #
 $VERBOSE = nil
 
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
 
 require 'pry'
 require 'sidekiq/testing'
